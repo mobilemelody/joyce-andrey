912f1aa2a1bfa596c47a32b8ab171680e6675381
mobilemelody <melody.reebs@gmail.com>
2019-11-09 17:50:04 -0800

    IE fixes, added credits

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const pug = require('pug');
 const bodyParser = require('body-parser');
+const modernizr = require("modernizr");
 const {google} = require('googleapis');
 
 const app = express();
